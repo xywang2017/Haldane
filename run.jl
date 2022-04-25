69512daf3d65425f2709069cbe05c9c640d47762
xywang2017 <40737927+xywang2017@users.noreply.github.com>
2022-04-25 09:44:34 -0400

    base

--- run.jl
+++ run.jl
@@ -1,15 +1,30 @@
 using PyPlot
+using DataFrame
+using CSV
 include("haldane.jl")
 fpath = pwd()
 
-ϕs = collect(1:96) .// 97
-for iϕ in eachindex(ϕs)
-    ϕ = ϕs[iϕ]
-    q = denominator(ϕ)
-    p = numerator(ϕ)
-    hd = Haldane()
-    fname = joinpath(fpath,"data/p$(p)q$(q).txt")
-    constructHaldane(hd;ϕ=ϕ,lk=4,fname=fname)
+function compute_spectrum()
+    ϕs = collect(1:96) .// 97
+    for iϕ in eachindex(ϕs)
+        ϕ = ϕs[iϕ]
+        q = denominator(ϕ)
+        p = numerator(ϕ)
+        hd = Haldane()
+        fname = joinpath(fpath,"data/p$(p)q$(q).txt")
+        constructHaldane(hd;ϕ=ϕ,lk=4,fname=fname)
+    end
+
+    # data processing 
+    df = DataFrame() 
+    for iϕ in eachindex(ϕs)
+        ϕ = ϕs[iϕ]
+        q = denominator(ϕ)
+        p = numerator(ϕ)
+        df[!,"$p_$q"] = readdlm(joinpath(fpath,"data/p$(p)q$(q).txt"))
+    end
+    out_name = joinpath(fpath,"data/q$q.csv")
+    CSV.write(out_name,df,delim=",")
 end
 
 ##
